e3c8963e155b6ce620d1a8dc6b2038b69a83ba8a
pc <pc@10.0.0.103>
2023-04-07 18:06:09 -0300

    commit initial

---
+++ Programando/src/Empresa/Principal.java
@@ -0,0 +1,14 @@
+package Empresa;
+
+import java.util.Date;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Date relogio = new Date();
+	    System.out.println("A hora do sistema");
+	    System.out.println(relogio.toString());
+	}
+
+}
